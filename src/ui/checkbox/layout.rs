322a7d2d7c085c83460968cc4fc96ade49a240ac
Евгений <sudakovzhenya02@gmail.com>
2026-03-01 23:37:33 +0700

    Remove alignment feature from LayoutCheckboxGroup for simplification

--- src/ui/checkbox/layout.rs
+++ src/ui/checkbox/layout.rs
@@ -1,26 +1,22 @@
 // src/ui/checkbox/layout.rs
 
 use crate::ui::checkbox::Checkbox;
-use ratatui::layout::Alignment;
 
-#[allow(dead_code)]
 pub trait LayoutCheckboxGroup<'a>
 {
     fn new() -> Self;
     fn add_checkbox(&mut self, checkbox: Checkbox<'a>);
-    fn alignment(&mut self, value: Alignment);
 }
 
 pub struct LayoutCheckboxGroupData<'a>
 {
     pub checkboxes: Vec<Checkbox<'a>>,
-    pub alignment: Alignment,
 }
 
 impl<'a> Default for LayoutCheckboxGroupData<'a>
 {
     fn default() -> Self
     {
-        return Self { checkboxes: Vec::new(), alignment: Alignment::Left };
+        return Self { checkboxes: Vec::new() };
     }
 }
